748ef020945849a35e428b2021a25f33cd016dec
Chibuoyim <onuigwechibuoyim@gmail.com>
2023-01-01 17:23:33 +0100

    updated token

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,7 @@ const WhatsappCloudAPI = require('whatsappcloudapi_wrapper');
 const Whatsapp = new WhatsappCloudAPI({
     accessToken: process.env.Meta_WA_accessToken,
     senderPhoneNumberId: process.env.Meta_WA_SenderPhoneNumberId,
-    WABA_ID: process.env.Meta_WA_wabaId,
-    graphAPIVersion: 'v14.0'
+    WABA_ID: process.env.Meta_WA_wabaId
 });
 
 const EcommerceStore = require('./../utils/ecommerce_store.js');
